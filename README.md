9f54981fb5024ff1525decae464456c68b0c4ba2
Clinton Graham <ctgraham@pitt.edu>
2024-07-03 15:55:39 -0400

    Add README description, finish draft of shell process

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ArchivesSpace-EAD-Update
+
+The ULS updated ArchivesSpace records via SQL to deduplicate container ids recorded there.  As a result, we need to reload each modified Finding Aid within our legacy Islandora 7 instance.
+
+This code takes in a list of affected ASpace records, downloads all Finding Aids from Islandora 7, searches for the EAD identifier in the downloaded files, downloads the matched EAD by EAD ID from ASpace, and then uploads the changed EADs back to Islandora 7.
--- update_eads_for_duplicated_ref_ids.sh
+++ update_eads_for_duplicated_ref_ids.sh
@@ -1,3 +1,10 @@
+# USAGE: $0 [filename]
+# filename: list of changed ASpace records on individual lines
+# format for ASpace input is resourse id, repository id, and EAD id, whitespace separated.
+# STDIN prompts:
+# subdomain: pittsbapi or pittapi
+# password: for sysdevapi
+
 # Constants
 SCHEME="https://"
 ASPACE_LOGIN_URL=".as.atlas-sys.com/users/sysdevapi/login"
@@ -44,16 +51,20 @@ formatLoginURL() {
 formatEAD_URL() {
 	local repo_id=$1;
 	local resource_id=$2;
-	local ending=`addResourceID $repo_id $resource_id`;
+	local ending=`addResourceID $repo_id $resource_id`'?include_unpublished=false&include_daos=true&numbered_cs=true&print_pdf=false&ead3=false';
 	formatURL $ending
 }
+
+if [[ "$1" == "" ]]
+then
+  echo "Usage: $0 <filename>"
+  echo "  filename: input file of EAD IDs"
+  exit 1
+fi
 
 # Hold onto current tty status to hide password input and re-enable input
 stty_orig=$(stty -g)
 trap "stty ${stty_orig}" EXIT
-
-# Get User to run islandora_datastream_crud as
-read -p "Islandora_Datastream_Crud User: " USER
 
 # Get Subdomain for URL
 read -p "subdomain: " subDomain
@@ -72,7 +83,10 @@ echo `formatLoginURL`
 
 # Create temporary directory
 tmpdir=$(mktemp -d)
-dir=$(mkdir "${tmpdir}/EADs")
+workdir="${tmpdir}/EADs"
+mkdir $workdir
+updatedir="${tmpdir}/updates"
+mkdir $updatedir
 
 # Use drush and islandora_datastream_crud to get EADs
 drush --root=/var/www/html/drupal7/ --user=$USER --uri=http://gamera.library.pitt.edu \
@@ -81,8 +95,9 @@ islandora_datastream_crud_fetch_pids \
 --pid_file=$tmpdir/eadpids.txt
 
 
-drush islandora_datastream_crud_fetch_datastreams --user=$USER --pid_file=$tmpdir/eadpids.txt \
---dsid=EADS --datastreams_directory=$dir
+drush -qy --root=/var/www/html/drupal7/ --user=$USER --uri=http://gamera.library.pitt.edu \
+islandora_datastream_crud_fetch_datastreams --pid_file=$tmpdir/eadpids.txt \
+--dsid=EAD --datastreams_directory=$workdir --filename_separator=^
 
 # Curl to get Session ID
 # response is a json object
@@ -90,22 +105,31 @@ response=$(curl -s -X POST -F password=$password `formatLoginURL`)
 
 # Make sure user correctly loged in, else exit early
 if containsError; then
+	>&2 echo "ASpace Login Error: $response"
 	exit 1
 fi
 
 if containsSession; then
 	session=$(getSessionID $response)
 	echo $session
+	{
+	read
 	while IFS= read -r line; do
 		IFS='	'
 		read -r resourceID repoID EADID <<< "$line"
-		echo $resourceID
-		echo $repoID
-		echo $EADID >> "${tmpdir}/ead_ids.txt"
-		#curl -s -H "X-ArchivesSpace-Session: $session" `formatEAD_URL $repoID $resourceID` > "${dir}/${resourceID}_EAD.xml"
-	done < "$1"
-	#curl -H "X-ArchivesSpace-Session: $SESSION" `formatEAD_URL
+		updatedEAD=`grep -rl '>'$EADID'</eadid>' $workdir | grep -v -- '-test' | sed 's/EADs/updates/'`
+		if [[ "$updatedEAD" ]]
+		then
+			wget -qO "$updatedEAD" --header "X-ArchivesSpace-Session: $session" "`formatEAD_URL $repoID $resourceID`"
+		fi
+	done
+	} < "$1"
+	for i in $updatedir/*.xml; do xmllint --noout $i; done
+	echo drush -qy --root=/var/www/html/drupal7/ --user=$USER --uri=http://gamera.library.pitt.edu \
+islandora_datastream_crud_push_datastreams \
+--datastreams_source_directory=$updatedir --filename_separator=^
 	exit 0
 fi
 
+>&2 echo "No ASpace Session: $response"
 exit 1
